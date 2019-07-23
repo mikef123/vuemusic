d95017f8124770679ac13a8be391ca71da890e72
mikef123 <miguelferu@gmail.com>
2019-07-22 23:38:07 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+    appname: 'vuemusic',
+    apikey: 'd364112848e02e5d91906cc263eb287b',
+    secret: '4d657ecbdf29d1f605a1997738e4c218',
+    registeredTo: 'mik-rec'
+}
+
+
+// Application name	vuemusic
+// API key	d364112848e02e5d91906cc263eb287b
+// Shared secret	4d657ecbdf29d1f605a1997738e4c218
+// Registered to	mik-rec
